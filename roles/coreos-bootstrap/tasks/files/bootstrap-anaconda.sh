c9765ba25f30d35f2180b78419178c4715f60abe
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-10 03:50:48 +1000

    [coreos-bootstrap] Use /home/core/.bootstrapped flag

--- roles/coreos-bootstrap/tasks/files/bootstrap-anaconda.sh
+++ roles/coreos-bootstrap/tasks/files/bootstrap-anaconda.sh
@@ -5,3 +5,4 @@ cd /home/core
 wget https://repo.continuum.io/archive/Anaconda2-4.4.0-Linux-x86_64.sh
 bash Anaconda2-4.4.0-Linux-x86_64.sh -b
 rm Anaconda2-4.4.0-Linux-x86_64.sh
+touch .bootstrapped
--- roles/coreos-bootstrap/tasks/files/bootstrap-vagrant.sh
+++ roles/coreos-bootstrap/tasks/files/bootstrap-vagrant.sh
@@ -1,18 +1,6 @@
 #!/bin/bash -uxe
 
-VERSION=2.7.13.2715
-PACKAGE=ActivePython-${VERSION}-linux-x86_64-glibc-2.12-402695
+cd /home/core
 
-# make directory
-mkdir -p /opt/bin
-cd /opt
-
-# tar -xzf ${PACKAGE}.tar.gz > /dev/null
-
-mv ${PACKAGE} apy && cd apy && ./install.sh -I /opt/python/
-
-ln -sf /opt/python/bin/easy_install /opt/bin/easy_install
-ln -sf /opt/python/bin/pip /opt/bin/pip
-ln -sf /opt/python/bin/python /opt/bin/python
-ln -sf /opt/python/bin/python /opt/bin/python2
-ln -sf /opt/python/bin/virtualenv /opt/bin/virtualenv
+bash opt/Anaconda2-4.4.0-Linux-x86_64.sh -b
+touch .bootstrapped
