54222ca0ca1c30022334570720ba779be090f71c
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-10 03:52:34 +1000

    [coreos-bootstrap] Fix files/bootstrap-vagrant.sh

--- roles/coreos-bootstrap/tasks/files/bootstrap-vagrant.sh
+++ roles/coreos-bootstrap/tasks/files/bootstrap-vagrant.sh
@@ -2,5 +2,6 @@
 
 cd /home/core
 
-bash opt/Anaconda2-4.4.0-Linux-x86_64.sh -b
+cp opt/Anaconda2-4.4.0-Linux-x86_64.sh ./Anaconda2-4.4.0-Linux-x86_64.sh
+bash ./Anaconda2-4.4.0-Linux-x86_64.sh -b
 touch .bootstrapped
