e9a815b57b86d772f5bd66214298cad6de9e8360
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-10 04:27:38 +1000

    [coreos-bootstrap][vagrant] Remove reduant copy

--- roles/coreos-bootstrap/tasks/files/bootstrap-vagrant.sh
+++ roles/coreos-bootstrap/tasks/files/bootstrap-vagrant.sh
@@ -2,6 +2,5 @@
 
 cd /home/core
 
-cp opt/Anaconda2-4.4.0-Linux-x86_64.sh ./Anaconda2-4.4.0-Linux-x86_64.sh
-bash ./Anaconda2-4.4.0-Linux-x86_64.sh -b
+bash opt/Anaconda2-4.4.0-Linux-x86_64.sh -b
 touch .bootstrapped
