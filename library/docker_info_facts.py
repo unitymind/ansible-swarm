ec3073daf3fb77aa82f3d468f681b7990ae8e7d6
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-12 01:58:44 +1000

    [docker-swarm-grouping] Switch to using docker_info_facts module

--- library/docker_info_facts.py
+++ library/docker_info_facts.py
@@ -28,44 +28,34 @@ EXAMPLES = """
   docker_info_facts:
 """
 
-docker_lib_missing=False
 
-try:
-    from docker import Client
-except:
+from ansible.module_utils.docker_common import AnsibleDockerClient
+
+
+def _get_docker_info(client):
     try:
-        from docker import APIClient as Client
-    except:
-        docker_lib_missing=True
-
-
-def _get_docker_info():
-    try:
-        return Client().info(), False
+        return client.info(), False
     except Exception as e:
         return {}, e.message
 
 
 def main():
-    module = AnsibleModule(
-        argument_spec=dict(),
+    argument_spec = dict()
+
+    client = AnsibleDockerClient(
+        argument_spec=argument_spec,
         supports_check_mode=False
     )
 
-    if docker_lib_missing:
-        msg = "Could not load docker python library; please install docker-py or docker library"
-        module.fail_json(msg=msg)
-
-    info, err = _get_docker_info()
+    info, err = _get_docker_info(client)
 
     if err:
-        module.fail_json(msg=err)
+        client.module.fail_json(msg=err)
 
-    module.exit_json(
+    client.module.exit_json(
         changed=True,
-        ansible_facts={'docker_info': info})
+        ansible_facts={'docker_info': info}
+    )
 
-
-from ansible.module_utils.basic import *
 if __name__ == '__main__':
     main()
