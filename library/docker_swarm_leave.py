599c8177d40c67c6c65ef730947dcd3138014333
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-12 08:02:06 +1000

    [library/docker_swarm_leave] Introduce module and update [docker-swarm-leave] role

--- library/docker_swarm_leave.py
+++ library/docker_swarm_leave.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2016, Cleawing, LLC.
+# Copyright 2017, Cleawing, LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,41 +15,56 @@
 
 DOCUMENTATION = """
 ---
-module: docker_info_facts
+module: docker_swarm_leave
 short_description:
-    - A module for injecting Docker info as facts.
+    - A module for leaving node from a cluster.
 description:
-    - A module for injecting Docker info as facts.
+    - A module for leaving node from a cluster.
 author: unitymind
+
+options:
+  force:
+    description:
+      - Leave the swarm even if this node is a manager.
+    required: False
+    default: False
+    
+extends_documentation_fragment:
+    - docker
 """
 
 EXAMPLES = """
-- name: load docker info facts
-  docker_info_facts:
+- name: Leave node from a cluster
+  docker_swarm_leave:
+  
+- name: Force leave node from a cluster
+  docker_swarm_leave:
+    force: True
 """
 
 
 from ansible.module_utils.docker_common import AnsibleDockerClient, DockerBaseClass
 
 
-class InfoManager(DockerBaseClass):
+class SwarmLeaveManager(DockerBaseClass):
 
     def __init__(self, client, results):
 
-        super(InfoManager, self).__init__()
+        super(SwarmLeaveManager, self).__init__()
 
         self.client = client
         self.results = results
         self.check_mode = self.client.check_mode
+        parameters = self.client.module.params
+
+        self.force = parameters.get("force")
 
         self.execute()
 
     def execute(self):
         try:
-            if not self.check_mode:
-                info = self.client.info()
+            if self.client.swarm.leave(self.force):
                 self.results['changed'] = True
-                self.results['ansible_facts'] = {'docker_info': info}
         except Exception as e:
             self.fail(e.message)
 
@@ -58,19 +73,20 @@ class InfoManager(DockerBaseClass):
 
 
 def main():
-    argument_spec = dict()
+    argument_spec = dict(
+        force=dict(type='bool', default=False)
+    )
 
     client = AnsibleDockerClient(
         argument_spec=argument_spec,
-        supports_check_mode=True
+        supports_check_mode=False
     )
 
     results = dict(
-        changed=False,
-        ansible_facts={}
+        changed=False
     )
 
-    InfoManager(client, results)
+    SwarmLeaveManager(client, results)
     client.module.exit_json(**results)
 
 if __name__ == '__main__':
