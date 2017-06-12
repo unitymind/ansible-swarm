7b3653d37edb4d4cde49b37a5a884345907dfbfb
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-12 23:57:52 +1000

    [bootstrap-coreos] Install miniconda with /opt prefix

--- roles/bootstrap-coreos/tasks/files/remote.sh
+++ roles/bootstrap-coreos/tasks/files/remote.sh
@@ -3,6 +3,6 @@
 cd $HOME
 
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh -b
+bash Miniconda2-latest-Linux-x86_64.sh -b -p /opt
 rm Miniconda2-latest-Linux-x86_64.sh
 touch .bootstrapped
--- roles/bootstrap-coreos/tasks/files/vagrant.sh
+++ roles/bootstrap-coreos/tasks/files/vagrant.sh
@@ -2,5 +2,5 @@
 
 cd $HOME
 
-bash opt/Miniconda2-latest-Linux-x86_64.sh -b
+bash opt/Miniconda2-latest-Linux-x86_64.sh -b -p /opt
 touch .bootstrapped
