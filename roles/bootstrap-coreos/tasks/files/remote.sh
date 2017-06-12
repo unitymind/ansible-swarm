b9ee09ef0775a5d27ef762f4ec3b741e752d060d
Vitaliy V. Shopov <vitaliy.shopov@cleawing.com>
2017-06-13 00:07:53 +1000

    [bootstrap-rexray] Update

--- roles/bootstrap-coreos/tasks/files/remote.sh
+++ roles/bootstrap-coreos/tasks/files/remote.sh
@@ -3,6 +3,5 @@
 cd $HOME
 
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh -b -p /opt
+bash Miniconda2-latest-Linux-x86_64.sh -b
 rm Miniconda2-latest-Linux-x86_64.sh
-touch .bootstrapped
--- roles/bootstrap-coreos/tasks/files/vagrant.sh
+++ roles/bootstrap-coreos/tasks/files/vagrant.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -uxe
 
 cd $HOME
-
-bash opt/Miniconda2-latest-Linux-x86_64.sh -b -p /opt
-touch .bootstrapped
+bash opt/Miniconda2-latest-Linux-x86_64.sh -b
